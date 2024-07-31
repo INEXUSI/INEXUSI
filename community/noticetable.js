f3d65015aee09fd1f41dc911074340986300161f
inexusi <admin@inexusi.com>
2024-07-31 18:02:54 +0900

    .

--- community/noticetable.js
+++ community/noticetable.js
@@ -1,8 +1,5 @@
 const itemDB = [
-    {num : "1", onoff:"on",date : "2023/11/17", title : "ESG 활동 선도하는 윤태근 회장, 사회 지도자들과의 만남", img : "https://inexusi.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Fc31b3a0f-b3c5-4eed-8dbc-3f8e396e6c32%2F18aa1ea1-53b4-427a-8c67-cecc769463fa%2FUntitled.png?table=block&id=43806de4-47ff-4035-860c-74ed6c6d04d7&spaceId=c31b3a0f-b3c5-4eed-8dbc-3f8e396e6c32&width=630&userId=&cache=v2"},
-    {num : "2", onoff:"on",date : "2023/12/01", title : "몽골 국방부 장관(Gursediin Saikhanbayar)과의 만남", img : "https://inexusi.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Fc31b3a0f-b3c5-4eed-8dbc-3f8e396e6c32%2F3075c1c3-2522-4f1f-b560-70d99785b4dd%2FUntitled.png?table=block&id=296df04c-20de-4565-a6d5-855d620bed47&spaceId=c31b3a0f-b3c5-4eed-8dbc-3f8e396e6c32&width=630&userId=&cache=v2"},
-    {num : "3", onoff:"off",date : "2023/12/01", title : "박상철 국회 입법 조사처장과 만남", img : "https://inexusi.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Fc31b3a0f-b3c5-4eed-8dbc-3f8e396e6c32%2F02300b00-87b1-4c44-9e5e-3d26f5669c4e%2FUntitled.png?table=block&id=c6317c5a-e376-4988-b19b-51d90041e087&spaceId=c31b3a0f-b3c5-4eed-8dbc-3f8e396e6c32&width=630&userId=&cache=v2"},
-    {num : "4", onoff:"on",date : "2023/12/02", title : "대한민국 공군 참모총장 만남", img : "https://inexusi.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Fc31b3a0f-b3c5-4eed-8dbc-3f8e396e6c32%2F016ff1a0-89b7-4359-be87-9e7001186a7f%2FUntitled.png?table=block&id=2c647f3e-954f-4d27-8d27-152782d623f4&spaceId=c31b3a0f-b3c5-4eed-8dbc-3f8e396e6c32&width=640&userId=&cache=v2"},
+    {num : "1", onoff:"off",date : "", title : " ", img : ""},
     {num : "", onoff:"off",date : "", title : "샘플", img : ""},
     {num : "", onoff:"off",date : "", title : "샘플", img : ""},
 ]
